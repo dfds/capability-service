3c5ee9af2cdcd356cc804b9753dfdd2747133ef8
Kim Lindhard <kilin@dfds.com>
2018-10-08 13:29:47 +0200

    replace lauchsettings with start-webapi script

---
+++ start-webapi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd src/TeamService.WebApi
+
+AWS_COGNITO_ACCESS_KEY="" \
+AWS_COGNITO_SECRET_ACCESS_KEY="" \
+AWS_COGNITO_USER_POOL_ID="" \
+AWS_COGNITO_IDENTITY_POOL_ID_KEY="" \
+AWS_COGNITO_USER_POOL_PROVIDER_KEY="" \
+AWS_COGNITO_LOGIN_PROVIDER_NAME="" \
+AWS_COGNITO_IDENTITY_POOL_ID="" \
+ASPNETCORE_URLS=http://+:8080 \
+dotnet watch run --no-launch-profile
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd SCRIPT-DIR
