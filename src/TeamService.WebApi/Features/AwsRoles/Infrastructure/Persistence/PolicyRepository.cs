7780b3fd16c8d4a82678b789a546aa0e5bcbcd06
Kim Lindhard <kilin@dfds.com>
2018-11-02 11:55:00 +0100

    read policy from json file

--- src/TeamService.WebApi/Features/AwsRoles/Infrastructure/Persistence/PolicyRepository.cs
+++ src/TeamService.WebApi/Features/AwsRoles/Infrastructure/Persistence/PolicyRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using DFDS.TeamService.WebApi.Features.AwsRoles.Model;
 
@@ -6,32 +7,33 @@ namespace DFDS.TeamService.WebApi.Features.AwsRoles.Infrastructure.Persistence
 {
     public class PolicyRepository : IPolicyRepository
     {
-        public Task<IEnumerable<Policy>> GetLatestAsync()
+        public async Task<IEnumerable<Policy>> GetLatestAsync()
         {
-            return Task.FromResult(GetLatest());
-        }
-        
-        
-        public IEnumerable<Policy> GetLatest()
-        {
+            var policyDocument = await ReadFileAsync("rds-all-v001.json");
+
             var rdsPolicy = new Policy(
                 "RDS-All",
-                @"{
-                       ""Version"": ""2012-10-17"",
-                       ""Statement"": [
-                           {
-                               ""Effect"": ""Allow"",
-                               ""Action"": ""rds:*"",
-                               ""Resource"": ""*""
-                           }
-                       ]
-                    }"
+                policyDocument
             );
-
-            var policies = new [] {rdsPolicy};
-
             
+            
+            var policies = new[] {rdsPolicy};
+
+
             return policies;
+        }
+
+        
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            var baseFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var fileFolder = Path.Combine(baseFolder, "Features/AwsRoles/Infrastructure/Persistence/Policies");
+            var filePath = Path.Combine(fileFolder, fileName);
+          
+            var fileContent= await File.ReadAllTextAsync(filePath);
+
+
+            return fileContent;
         }
     }
 }
