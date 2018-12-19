af672810721b1bf6fab1bfd43911ecc561446bb3
Jens Andresen <jandr@DFDS.COM>
2018-12-19 14:14:29 +0100

    add hello world controller

---
+++ src/TeamService.WebApi/Controllers/TeamController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFDS.TeamService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/teams")]
+    public class TeamController  : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult GetAllTeams()
+        {
+            return Ok("hello world");
+        }
+    }
+}
