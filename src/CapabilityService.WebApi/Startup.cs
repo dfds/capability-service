61b15fd9a791276896762ebfd48f70dec46d3c3e
Thomas Wittrup Fischer <twf@trifork.com>
2019-02-15 12:41:00 +0100

    expose /healthz endpoint

--- src/CapabilityService.WebApi/Startup.cs
+++ src/CapabilityService.WebApi/Startup.cs
@@ -7,12 +7,16 @@ using DFDS.CapabilityService.WebApi.Domain.Repositories;
 using DFDS.CapabilityService.WebApi.Infrastructure.Integrations;
 using DFDS.CapabilityService.WebApi.Infrastructure.Messaging;
 using DFDS.CapabilityService.WebApi.Infrastructure.Persistence;
+using HealthChecks.Uris;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Prometheus;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
@@ -34,29 +38,32 @@ namespace DFDS.CapabilityService.WebApi
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            var connectionString = Configuration["CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING"];
+            
             services
                 .AddEntityFrameworkNpgsql()
                 .AddDbContext<CapabilityServiceDbContext>((serviceProvider, options) =>
                 {
-                    var connectionString = Configuration["CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING"];
                     options.UseNpgsql(connectionString);
                 });
 
+            var iamRoleServiceUrl = Configuration["IAMROLESERVICE_URL"];
+
             services.AddHttpClient<IAMRoleServiceClient>(cfg =>
             {
-                var baseUrl = Configuration["IAMROLESERVICE_URL"];
-                if (baseUrl != null)
+                if (iamRoleServiceUrl != null)
                 {
-                    cfg.BaseAddress = new Uri(baseUrl);
+                    cfg.BaseAddress = new Uri(iamRoleServiceUrl);
                 }
             });
 
+            var roleMapperServiceUrl = Configuration["ROLEMAPPERSERVICE_URL"];
+            
             services.AddHttpClient<RoleMapperServiceClient>(cfg =>
             {
-                var baseUrl = Configuration["ROLEMAPPERSERVICE_URL"];
-                if (baseUrl != null)
+                if (roleMapperServiceUrl != null)
                 {
-                    cfg.BaseAddress = new Uri(baseUrl);
+                    cfg.BaseAddress = new Uri(roleMapperServiceUrl);
                 }
             });
 
@@ -75,6 +82,13 @@ namespace DFDS.CapabilityService.WebApi
 
             ConfigureDomainEvents(services);
 			services.AddHostedService<MetricHostedService>();
+
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddNpgSql(connectionString, tags: new[] {"backing services", "postgres"})
+                .AddUrlGroup(new Uri(iamRoleServiceUrl), name: "iam_role_service", failureStatus: HealthStatus.Degraded, tags: new[] {"backing services", "role", "iam"})
+                .AddUrlGroup(new Uri(roleMapperServiceUrl), name: "role_mapper_service", failureStatus: HealthStatus.Degraded, tags: new[] {"backing services", "role", "mapper"})
+                ;
         }
 
         private static void ConfigureDomainEvents(IServiceCollection services)
@@ -100,6 +114,11 @@ namespace DFDS.CapabilityService.WebApi
             }
 
             app.UseMvc();
+            
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                ResponseWriter = MyPrometheusStuff.WriteResponseAsync
+            });
         }
     }
 
@@ -126,6 +145,53 @@ namespace DFDS.CapabilityService.WebApi
                 Console.WriteLine("Shutting down metric server");
                 await _metricServer.StopAsync();
                 Console.WriteLine("Done shutting down metric server");
+            }
+        }
+    }
+    
+    public static class MyPrometheusStuff
+    {
+        private const string HealthCheckLabelServiceName = "service";
+        private const string HealthCheckLabelStatusName = "status";
+
+        private static readonly Gauge HealthChecksDuration;
+        private static readonly Gauge HealthChecksResult;
+
+        static MyPrometheusStuff()
+        {
+            HealthChecksResult = Metrics.CreateGauge("healthcheck",
+                "Shows health check status (status=unhealthy|degraded|healthy) 1 for triggered, otherwise 0", new GaugeConfiguration
+                {
+                    LabelNames = new[] {HealthCheckLabelServiceName, HealthCheckLabelStatusName},
+                    SuppressInitialValue = false
+                });
+
+            HealthChecksDuration = Metrics.CreateGauge("healthcheck_duration_seconds",
+                "Shows duration of the health check execution in seconds",
+                new GaugeConfiguration
+                {
+                    LabelNames = new[] {HealthCheckLabelServiceName},
+                    SuppressInitialValue = false
+                });
+        }
+
+        public static Task WriteResponseAsync(HttpContext httpContext, HealthReport healthReport)
+        {
+            UpdateMetrics(healthReport);
+
+            httpContext.Response.ContentType = "text/plain";
+            return httpContext.Response.WriteAsync(healthReport.Status.ToString());
+        }
+
+        private static void UpdateMetrics(HealthReport report)
+        {
+            foreach (var (key, value) in report.Entries)
+            {
+                HealthChecksResult.Labels(key, "healthy").Set(value.Status == HealthStatus.Healthy ? 1 : 0);
+                HealthChecksResult.Labels(key, "unhealthy").Set(value.Status == HealthStatus.Unhealthy ? 1 : 0);
+                HealthChecksResult.Labels(key, "degraded").Set(value.Status == HealthStatus.Degraded ? 1 : 0);
+
+                HealthChecksDuration.Labels(key).Set(value.Duration.TotalSeconds);
             }
         }
     }
