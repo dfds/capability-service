72194ac2563c9f81009916ddb8082d5a418ad373
Jens Andresen <jandr@DFDS.COM>
2019-02-18 10:46:39 +0100

    add test helper for comparing instance properties

---
+++ src/CapabilityService.Tests/Helpers/PropertiesComparer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DFDS.CapabilityService.Tests.Helpers
+{
+    public class PropertiesComparer<T> : IEqualityComparer<T>
+    {
+        public bool Equals(T x, T y)
+        {
+            if (x.GetType() != y.GetType())
+            {
+                return false;
+            }
+
+            var properties = x.GetType()
+                              .GetProperties()
+                              .ToArray();
+
+            return properties.All(p => Comparer.Default.Compare(p.GetValue(x), p.GetValue(y)) == 0);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
--- src/CapabilityService.Tests/TestCapability.cs
+++ src/CapabilityService.Tests/TestCapability.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+﻿using DFDS.CapabilityService.Tests.Helpers;
 using DFDS.CapabilityService.WebApi.Domain.Events;
 using DFDS.CapabilityService.WebApi.Domain.Models;
 using Xunit;
@@ -20,76 +15,8 @@ namespace DFDS.CapabilityService.Tests
             Assert.Equal(
                 expected: new[] {new CapabilityCreated(capability.Id, "foo")},
                 actual: capability.DomainEvents,
-                comparer: new DomainEventComparer()
+                comparer: new PropertiesComparer<IDomainEvent>()
             );
-        }
-    }
-
-    public class DomainEventComparer : IEqualityComparer<IDomainEvent>
-    {
-        public bool Equals(IDomainEvent x, IDomainEvent y)
-        {
-            if (x.GetType() != y.GetType())
-            {
-                return false;
-            }
-
-            //var id = x.GetPropertyInfo(e => e.EventId);
-
-            var properties = x.GetType()
-                              .GetProperties()
-                              //.Where(p => p.Name != id.Name)
-                              .ToArray();
-
-            return properties.All(p => Comparer.Default.Compare(p.GetValue(x), p.GetValue(y)) == 0);
-        }
-
-        public int GetHashCode(IDomainEvent obj)
-        {
-            return obj.GetHashCode();
-        }
-    }
-
-    public static class PropertySelector
-    {
-        public static PropertyInfo GetPropertyInfo<T>(this T obj, Expression<Func<T, object>> selector)
-        {
-            if (selector.NodeType != ExpressionType.Lambda)
-            {
-                throw new ArgumentException("Selector must be lambda expression", "selector");
-            }
-
-            var lambda = (LambdaExpression)selector;
-
-            var memberExpression = ExtractMemberExpression(lambda.Body);
-
-            if (memberExpression == null)
-            {
-                throw new ArgumentException("Selector must be member access expression", "selector");
-            }
-
-            if (memberExpression.Member.DeclaringType == null)
-            {
-                throw new InvalidOperationException("Property does not have declaring type");
-            }
-
-            return memberExpression.Member.DeclaringType.GetProperty(memberExpression.Member.Name);
-        }
-
-        private static MemberExpression ExtractMemberExpression(Expression expression)
-        {
-            if (expression.NodeType == ExpressionType.MemberAccess)
-            {
-                return ((MemberExpression)expression);
-            }
-
-            if (expression.NodeType == ExpressionType.Convert)
-            {
-                var operand = ((UnaryExpression)expression).Operand;
-                return ExtractMemberExpression(operand);
-            }
-
-            return null;
         }
     }
 }
