18a033470521e0b802575e5dadb0d56a15812326
Kim Lindhard <kilin@dfds.com>
2019-04-25 14:45:41 +0200

    watch run script will use port 50900

--- local-development/watch-run.sh
+++ local-development/watch-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 IAMROLESERVICE_URL=http://localhost:50800 \
 ROLEMAPPERSERVICE_URL=http://localhost:50801 \
-CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING=ID=postgres;Password=p;Host=localhost;Port=5432;Database=capabilitydb; \
+CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING="User ID=postgres;Password=p;Host=localhost;Port=5432;Database=capabilitydb;" \
 CAPABILITY_SERVICE_KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
-dotnet watch --project ./../src/CapabilityService.WebApi/CapabilityService.WebApi.csproj run
+dotnet watch --project ./../src/CapabilityService.WebApi/CapabilityService.WebApi.csproj \
+run --server.urls "http://*:50900" --no-launch-profile
