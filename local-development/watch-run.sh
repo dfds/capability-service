1b8feda4aa76b3f657d1b85bc0db01844481a528
Jan Wiberg <jawib@dfds.com>
2019-07-08 16:26:08 +0200

    Add env var key for capability topic name

--- local-development/watch-run.sh
+++ local-development/watch-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING="User ID=postgres;Password=p;Host=localhost;Port=5432;Database=capabilitydb;" \
 CAPABILITY_SERVICE_KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
+CAPABILITY_SERVICE_KAFKA_TOPIC_CAPABILITY="build.capabilities" \
 dotnet watch --project ./../src/CapabilityService.WebApi/CapabilityService.WebApi.csproj \
 run --server.urls "http://*:50900" --no-launch-profile
