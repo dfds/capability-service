a8a559a1dbe0667eb51ae80e03240872fd43dab2
Emil H. Clausen <emcla@dfds.com>
2019-08-12 15:31:40 +0200

    test: Ensure MessagingMessageIncomprehensible is thrown when expected

---
+++ src/CapabilityService.Tests/Builders/EventDispatcherBuilder.cs
@@ -0,0 +1,44 @@
+using DFDS.CapabilityService.WebApi.Infrastructure.Messaging;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace DFDS.CapabilityService.Tests.Builders
+{
+    public class EventDispatcherBuilder
+    {
+        private ILogger<EventDispatcher> _logger;
+        private DomainEventRegistry _eventRegistry;
+        private EventHandlerFactory _eventHandlerFactory;
+        
+        public EventDispatcherBuilder()
+        {
+            _logger = new Logger<EventDispatcher>(new NullLoggerFactory());
+            _eventRegistry = new DomainEventRegistry();
+            _eventHandlerFactory = new EventHandlerFactory();
+        }
+
+        public EventDispatcherBuilder WithLogger(ILogger<EventDispatcher> logger)
+        {
+            _logger = logger;
+            return this;
+        }
+
+        public EventDispatcherBuilder WithEventRegistry(DomainEventRegistry eventRegistry)
+        {
+            _eventRegistry = eventRegistry;
+            return this;
+        }
+        
+        public EventDispatcherBuilder WithEventHandlerFactory(EventHandlerFactory eventHandlerFactory)
+        {
+            _eventHandlerFactory = eventHandlerFactory;
+            return this;
+        }
+
+
+        public EventDispatcher Build()
+        {
+            return new EventDispatcher(_logger, _eventRegistry, _eventHandlerFactory);
+        }
+    }
+}
---
+++ src/CapabilityService.Tests/Infrastructure/Messaging/TestEventDispatcher.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using DFDS.CapabilityService.Tests.Builders;
+using DFDS.CapabilityService.WebApi.Infrastructure.Messaging;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace DFDS.CapabilityService.Tests.Infrastructure.Messaging
+{
+    public class TestEventDispatcher
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("test-string")]
+        [InlineData("{xaxa}")]
+        public async Task throws_expected_exception_when_invalid_message(string generalDomainEventJson)
+        {
+            var sut = new EventDispatcherBuilder().Build();
+
+            await Assert.ThrowsAsync<MessagingMessageIncomprehensible>(async () => await sut.Send(generalDomainEventJson, null));
+        }
+    }
+}
