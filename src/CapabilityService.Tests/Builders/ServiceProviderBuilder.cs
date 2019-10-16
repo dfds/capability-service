5d1098dc707abf09f22f63f62faed960f8c3cb39
Kim Lindhard <kilin@dfds.com>
2019-10-16 06:09:24 +0200

    tests: add builder for service provider

---
+++ src/CapabilityService.Tests/Builders/ServiceProviderBuilder.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+using DFDS.CapabilityService.WebApi;
+using DFDS.CapabilityService.WebApi.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DFDS.CapabilityService.Tests.Builders
+{
+    public class ServiceProviderBuilder
+    {
+        readonly IServiceCollection _serviceCollection = new ServiceCollection();
+
+        
+        public ServiceProviderBuilder WithServicesFromStartup()
+        {
+            var configuration = new ConfigurationBuilder().Build();
+
+            var startup = new Startup(configuration);
+            startup.ConfigureServices(_serviceCollection);
+
+            
+            return this;
+        }
+        
+        public ServiceProviderBuilder WithInMemoryDb()
+        {
+            RemoveDbContextOptionsBuilder();
+            RemoveCapabilityServiceDbContext();
+            AddInMemoryCapabilityServiceDbContext();
+            
+            return this;
+        }
+        public IServiceProvider Build()
+        {
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+        
+        
+        private void RemoveCapabilityServiceDbContext()
+        {
+            var dbContextOptionsToRemove = _serviceCollection.FirstOrDefault(descriptor =>
+                descriptor.ServiceType == typeof(CapabilityServiceDbContext)
+            );
+            if (dbContextOptionsToRemove != null) _serviceCollection.Remove(dbContextOptionsToRemove);
+        }
+
+        private void AddInMemoryCapabilityServiceDbContext()
+        {
+            _serviceCollection
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<CapabilityServiceDbContext>((serviceProvider, options) =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                    options.ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                });
+        }
+
+        private void RemoveDbContextOptionsBuilder()
+        {
+            var dbContextOptionsToRemove = _serviceCollection.FirstOrDefault(descriptor =>
+                descriptor.ServiceType == typeof(DbContextOptions<CapabilityServiceDbContext>)
+            );
+            if (dbContextOptionsToRemove != null) _serviceCollection.Remove(dbContextOptionsToRemove);
+        }
+    }
+}
--- src/CapabilityService.Tests/Scenarios/DeleteCapabilityScenario.cs
+++ src/CapabilityService.Tests/Scenarios/DeleteCapabilityScenario.cs
@@ -1,36 +1,58 @@
+using System.Threading.Tasks;
+using DFDS.CapabilityService.Tests.Builders;
+using DFDS.CapabilityService.WebApi.Application;
+using DFDS.CapabilityService.WebApi.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace DFDS.CapabilityService.Tests.Scenarios
 {
     public class DeleteCapabilityScenario
     {
+        private Capability _capability;
+        private ICapabilityApplicationService _capabilityApplicationService;
+
+
         [Fact]
-        public void DeleteCapabilityRecipe()
+        public async Task DeleteCapabilityRecipe()
         {
-            Given_a_existing_capability();
-            When_delete_capability_is_posted();
+            await Given_a_service_collection_with_a_imMemoryDb();
+            await And_a_existing_capability();
+            await When_delete_capability_is_posted();
             Then_capability_deleted_event_is_emitted();
             And_capability_is_deleted_from_database();
         }
 
-        private void Given_a_existing_capability()
+        private async Task Given_a_service_collection_with_a_imMemoryDb()
         {
-            throw new System.NotImplementedException();
+            var serviceProviderBuilder = new ServiceProviderBuilder();
+           
+            var serviceProvider = serviceProviderBuilder
+                .WithServicesFromStartup()
+                .WithInMemoryDb()
+                .Build();
+
+            _capabilityApplicationService = serviceProvider.GetService<ICapabilityApplicationService>();
+
         }
 
-        private void When_delete_capability_is_posted()
+        private async Task And_a_existing_capability()
         {
-            throw new System.NotImplementedException();
+            _capability = await _capabilityApplicationService.CreateCapability("Foo", "This is a capability");
+        }
+
+        private async Task When_delete_capability_is_posted()
+        {
         }
 
         private void Then_capability_deleted_event_is_emitted()
         {
-            throw new System.NotImplementedException();
         }
 
         private void And_capability_is_deleted_from_database()
         {
-            throw new System.NotImplementedException();
         }
+        
+    
     }
 }
