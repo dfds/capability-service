1905a68926d2e857bfdbf42158bd35b1dbc7cd3e
Kim Lindhard <kilin@dfds.com>
2019-10-22 05:19:45 +0200

    add dependency check before starting PrometheusHealthCheck

--- src/CapabilityService.WebApi/Enablers/PrometheusHealthCheck/Middleware.cs
+++ src/CapabilityService.WebApi/Enablers/PrometheusHealthCheck/Middleware.cs
@@ -1,3 +1,5 @@
+using System;
+using CorrelationId;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -9,6 +11,9 @@ namespace DFDS.CapabilityService.WebApi.Enablers.PrometheusHealthCheck
             this IApplicationBuilder app
         )
         {
+            if (app.ApplicationServices.GetService(typeof (Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckService)) == null)
+                throw new InvalidOperationException("Unable to find the required services. You must call the AddHealthChecks method in ConfigureServices in the application startup code.");
+            
             app.UseHealthChecks("/healthz", new HealthCheckOptions
             {
                 ResponseWriter = PrometheusHealthProbe.WriteHealthResponseAsync
