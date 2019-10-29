15acafcf246cab198d20b0c6c4cc116fa743c5d7
Kim Lindhard <kilin@dfds.com>
2019-10-29 13:33:59 +0100

    registration of capability services happends in capabilities feature

---
+++ src/CapabilityService.WebApi/Features/Capabilities/Configuration/DependencyInjection.cs
@@ -0,0 +1,45 @@
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Application;
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Domain.Events;
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Domain.Repositories;
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Infrastructure.Events;
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Infrastructure.Persistence;
+using DFDS.CapabilityService.WebApi.Infrastructure.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DFDS.CapabilityService.WebApi.Features.Capabilities.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static void AddCapabilities(
+            this IServiceCollection services, 
+            IDomainEventRegistry domainEventRegistry,
+            string capabilitiesTopicName
+        )
+        {
+            services.AddTransient<ICapabilityRepository, CapabilityRepository>();
+            services.AddTransient<CapabilityOutboxEnabledDecorator>();
+            services.AddTransient<CapabilityApplicationService>();
+            services.AddTransient<ICapabilityApplicationService>(serviceProvider =>
+                new CapabilityTransactionalDecorator(
+                    inner: new CapabilityOutboxEnabledDecorator(
+                        inner: serviceProvider.GetRequiredService<CapabilityApplicationService>(),
+                        dbContext: serviceProvider.GetRequiredService<CapabilityServiceDbContext>(),
+                        outbox: serviceProvider.GetRequiredService<Outbox>()
+                    ),
+                    dbContext: serviceProvider.GetRequiredService<CapabilityServiceDbContext>()
+                ));
+            
+            
+            domainEventRegistry
+                .Register<CapabilityCreated>("capability_created", capabilitiesTopicName)
+                .Register<CapabilityUpdated>("capability_updated", capabilitiesTopicName)
+                .Register<CapabilityDeleted>("capability_deleted", capabilitiesTopicName)
+                .Register<MemberJoinedCapability>("member_joined_capability", capabilitiesTopicName)
+                .Register<MemberLeftCapability>("member_left_capability", capabilitiesTopicName)
+                .Register<ContextAddedToCapability>("context_added_to_capability", capabilitiesTopicName)
+                .Register<ContextUpdated>("context_updated", capabilitiesTopicName)
+                .Register<AWSContextAccountCreatedIntegrationEvent>("aws_context_account_created", capabilitiesTopicName);
+
+        }
+    }
+}
--- src/CapabilityService.WebApi/Startup.cs
+++ src/CapabilityService.WebApi/Startup.cs
@@ -8,20 +8,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Prometheus;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 using DFDS.CapabilityService.WebApi.Domain.EventHandlers;
-using DFDS.CapabilityService.WebApi.Domain.Events;
 using DFDS.CapabilityService.WebApi.Enablers.CorrelationId;
 using DFDS.CapabilityService.WebApi.Enablers.KafkaStreaming;
 using DFDS.CapabilityService.WebApi.Enablers.Metrics;
 using DFDS.CapabilityService.WebApi.Enablers.PrometheusHealthCheck;
-using DFDS.CapabilityService.WebApi.Features.Capabilities.Application;
-using DFDS.CapabilityService.WebApi.Features.Capabilities.Domain.Events;
-using DFDS.CapabilityService.WebApi.Features.Capabilities.Domain.Repositories;
+using DFDS.CapabilityService.WebApi.Features.Capabilities.Configuration;
 using DFDS.CapabilityService.WebApi.Features.Capabilities.Infrastructure.EventHandlers;
 using DFDS.CapabilityService.WebApi.Features.Capabilities.Infrastructure.Events;
 using DFDS.CapabilityService.WebApi.Features.Capabilities.Infrastructure.Persistence;
 using DFDS.CapabilityService.WebApi.Features.Topics.Configuration;
 using DFDS.CapabilityService.WebApi.Infrastructure.Messaging;
-using DFDS.CapabilityService.WebApi.Infrastructure.Persistence;
 
 namespace DFDS.CapabilityService.WebApi
 {
@@ -38,7 +34,7 @@ namespace DFDS.CapabilityService.WebApi
         {
             var connectionString = Configuration["CAPABILITYSERVICE_DATABASE_CONNECTIONSTRING"];
 
-            
+
             services
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -48,35 +44,33 @@ namespace DFDS.CapabilityService.WebApi
             services
                 .AddPrometheusHealthCheck()
                 .AddNpgSql(connectionString, tags: new[] {"backing services", "postgres"});
-                
 
-            ConfigureApplicationServices(services, connectionString);
 
-           
-            
+            ConfigureSharedServices(services, connectionString);
+
+
             var eventRegistry = new DomainEventRegistry();
             services.AddSingleton<IDomainEventRegistry>(eventRegistry);
-           
-
             services.AddSingleton(eventRegistry);
-            services.AddTransient<EventHandlerFactory>();
-            services.AddTransient<IEventHandler<AWSContextAccountCreatedIntegrationEvent>, AWSContextAccountCreatedEventHandler>();
-            services.AddTransient<IEventDispatcher, EventDispatcher>();
-            
+         
+
+            var capabilitiesTopicName = Configuration["CAPABILITY_SERVICE_KAFKA_TOPIC_CAPABILITY"];
+            services.AddCapabilities(
+                eventRegistry,
+                capabilitiesTopicName
+            );
+
             var capabilitiesTopicsTopicName = Configuration["CAPABILITY_SERVICE_KAFKA_TOPIC_TOPICS"];
             services.AddTopics(
                 eventRegistry,
                 capabilitiesTopicsTopicName
             );
-            
-            ConfigureCapabilitiesDomainEvents(eventRegistry);
-            
-            
+
             var scanner = new DomainEventScanner(eventRegistry);
             scanner.EnsureNoUnregisteredDomainEventsIn(Assembly.GetExecutingAssembly());
         }
 
-        private void ConfigureApplicationServices(IServiceCollection services, string connectionString)
+        private void ConfigureSharedServices(IServiceCollection services, string connectionString)
         {
             services
                 .AddEntityFrameworkNpgsql()
@@ -85,42 +79,24 @@ namespace DFDS.CapabilityService.WebApi
                     options.UseNpgsql(connectionString);
                 });
 
+            services.AddTransient<EventHandlerFactory>();
+            services
+                .AddTransient<IEventHandler<AWSContextAccountCreatedIntegrationEvent>,
+                    AWSContextAccountCreatedEventHandler>();
+            
+            services.AddTransient<IEventDispatcher, EventDispatcher>();
             services.AddTransient<Outbox>();
             services.AddTransient<DomainEventEnvelopeRepository>();
 
-            services.AddTransient<ICapabilityRepository, CapabilityRepository>();
-            services.AddTransient<CapabilityOutboxEnabledDecorator>();
-            services.AddTransient<CapabilityApplicationService>();
-            services.AddTransient<ICapabilityApplicationService>(serviceProvider => new CapabilityTransactionalDecorator(
-                inner: new CapabilityOutboxEnabledDecorator(
-                    inner: serviceProvider.GetRequiredService<CapabilityApplicationService>(),
-                    dbContext: serviceProvider.GetRequiredService<CapabilityServiceDbContext>(),
-                    outbox: serviceProvider.GetRequiredService<Outbox>()
-                ),
-                dbContext: serviceProvider.GetRequiredService<CapabilityServiceDbContext>()
-            ));
+      
 
-            services.AddTransient<IRepository<DomainEventEnvelope>,DomainEventEnvelopeRepository>();
-        }
-        private void ConfigureCapabilitiesDomainEvents(IDomainEventRegistry eventRegistry)
-        {
-            var capabilitiesTopicName = Configuration["CAPABILITY_SERVICE_KAFKA_TOPIC_CAPABILITY"];
-
-            eventRegistry
-                .Register<CapabilityCreated>("capability_created", capabilitiesTopicName)
-                .Register<CapabilityUpdated>("capability_updated", capabilitiesTopicName)
-                .Register<CapabilityDeleted>("capability_deleted", capabilitiesTopicName)
-                .Register<MemberJoinedCapability>("member_joined_capability", capabilitiesTopicName)
-                .Register<MemberLeftCapability>("member_left_capability", capabilitiesTopicName)
-                .Register<ContextAddedToCapability>("context_added_to_capability", capabilitiesTopicName)
-                .Register<ContextUpdated>("context_updated", capabilitiesTopicName)
-                .Register<AWSContextAccountCreatedIntegrationEvent>("aws_context_account_created", capabilitiesTopicName);
+            services.AddTransient<IRepository<DomainEventEnvelope>, DomainEventEnvelopeRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCorrelationId();
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
