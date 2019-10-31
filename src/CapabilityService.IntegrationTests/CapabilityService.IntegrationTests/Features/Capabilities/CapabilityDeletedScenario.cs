8a85284cb77a17078c745f7fa4fac0e660fbbe92
Kim Lindhard <kilin@dfds.com>
2019-10-31 10:41:14 +0100

    CapabilityApiClient can list & delete capabilities

--- src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/CapabilityDeletedScenario.cs
+++ src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/CapabilityDeletedScenario.cs
@@ -1,37 +1,42 @@
 using System;
 using System.Threading.Tasks;
 using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api;
+using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api.Model;
 using Xunit;
 
 namespace CapabilityService.IntegrationTests.Features.Capabilities
 {
     public class CapabilityDeletedScenario
     {
+        private CapabilityDto _capabilityDto;
+
         [Fact]
         public async Task CapabilityDeletedRecipe()
         {
             await Given_a_capability();
-            When_a_capability_is_deleted();
+            await When_a_capability_is_deleted();
             Then_it_wont_be_in_the_capability_list();
             And_a_event_will_be_published();
         }
 
         private async Task Given_a_capability()
         {
-            var name = "Integration-test-" + Guid.NewGuid().ToString();
+            var name = "Integration-test-" + Guid.NewGuid().ToString().Substring(0,8);
             var description = name;
 
-            await CapabilityApiClient.Capabilities.PostAsync(name, description);
+            _capabilityDto = await CapabilityApiClient.Capabilities.PostAsync(name, description);
         }
 
-        private void When_a_capability_is_deleted()
+        private async Task When_a_capability_is_deleted()
         {
-            throw new System.NotImplementedException();
+            await CapabilityApiClient.Capabilities.DeleteAsync(_capabilityDto.Id);
         }
 
-        private void Then_it_wont_be_in_the_capability_list()
+        private async Task Then_it_wont_be_in_the_capability_list()
         {
-            throw new System.NotImplementedException();
+            var capabilities = await CapabilityApiClient.Capabilities.GetAsync();
+            
+            Assert.All(capabilities.Items, dto => Assert.False(dto.Id.Equals(_capabilityDto.Id)));
         }
 
         private void And_a_event_will_be_published()
--- src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/Infrastructure/Api/CapabilityApiClient.cs
+++ src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/Infrastructure/Api/CapabilityApiClient.cs
@@ -2,48 +2,80 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api.Model;
 using Newtonsoft.Json;
 
 namespace CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api
 {
     public static class CapabilityApiClient
     {
-         public static class Capabilities
-         {
-             private const string CapabilitiesUrl = "http://localhost:50900/api/v1/capabilities";
+        public static class Capabilities
+        {
+            private const string CapabilitiesUrl = "http://localhost:50900/api/v1/capabilities";
 
-             public static async Task GetAsync()
-             {
-                 
-             }
+            public static async Task<ItemsEnvelope<CapabilityDto>> GetAsync()
+            {
+                var uri = new Uri(CapabilitiesUrl);
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
 
-             public static async Task PostAsync(string name, string description)
-             {
-                 var payload = new
-                 {
+                var httpClient = new HttpClient();
+                var responseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                var contentString = await responseMessage.Content.ReadAsStringAsync();
+
+                var deserializeObject = JsonConvert.DeserializeObject<ItemsEnvelope<CapabilityDto>>(contentString);
+
+                return deserializeObject;
+            }
+
+            public static async Task<CapabilityDto> PostAsync(string name, string description)
+            {
+                var payload = new
+                {
                     name = name,
                     description = description
-                 };
+                };
 
 
-                 var uri = new Uri(CapabilitiesUrl);
-                 var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+                var uri = new Uri(CapabilitiesUrl);
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
 
-                 httpRequestMessage.Content = new StringContent(
-                     JsonConvert.SerializeObject(payload),
-                     Encoding.UTF8,
-                     "application/json"
-                 );
+                httpRequestMessage.Content = new StringContent(
+                    JsonConvert.SerializeObject(payload),
+                    Encoding.UTF8,
+                    "application/json"
+                );
 
 
-                 var httpClient = new HttpClient();
-                 var responseMessage = await httpClient.SendAsync(httpRequestMessage);
+                var httpClient = new HttpClient();
+                var responseMessage = await httpClient.SendAsync(httpRequestMessage);
 
-                 if (responseMessage.IsSuccessStatusCode == false)
-                 {
-                     throw new Exception(responseMessage.ReasonPhrase);
-                 }
-             }
-         } 
+                if (responseMessage.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(responseMessage.ReasonPhrase);
+                }
+
+                var contentString = await responseMessage.Content.ReadAsStringAsync();
+
+                var deserializeObject = JsonConvert.DeserializeObject<CapabilityDto>(contentString);
+
+                return deserializeObject;
+            }
+
+            public static async Task DeleteAsync(Guid capabilityId)
+            {
+                var uri = new Uri(CapabilitiesUrl + "/" + capabilityId);
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
+
+                var httpClient = new HttpClient();
+                var responseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+
+                if (responseMessage.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(responseMessage.ReasonPhrase);
+                }
+            }
+        }
     }
 }
