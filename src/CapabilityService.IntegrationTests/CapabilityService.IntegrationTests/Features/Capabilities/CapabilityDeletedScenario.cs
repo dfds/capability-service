dd60b9501ca80cf20acc5b902fa1b4ea360de61b
Kim Lindhard <kilin@dfds.com>
2019-10-31 14:32:20 +0100

    add CapabilityKafkaClient

--- src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/CapabilityDeletedScenario.cs
+++ src/CapabilityService.IntegrationTests/CapabilityService.IntegrationTests/Features/Capabilities/CapabilityDeletedScenario.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api;
 using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Api.Model;
+using CapabilityService.IntegrationTests.Features.Capabilities.Infrastructure.Kafka;
 using Xunit;
 
 namespace CapabilityService.IntegrationTests.Features.Capabilities
@@ -15,13 +16,13 @@ namespace CapabilityService.IntegrationTests.Features.Capabilities
         {
             await Given_a_capability();
             await When_a_capability_is_deleted();
-            Then_it_wont_be_in_the_capability_list();
+            await Then_it_wont_be_in_the_capability_list();
             And_a_event_will_be_published();
         }
 
         private async Task Given_a_capability()
         {
-            var name = "Integration-test-" + Guid.NewGuid().ToString().Substring(0,8);
+            var name = "Integration-test-" + Guid.NewGuid().ToString().Substring(0, 8);
             var description = name;
 
             _capabilityDto = await CapabilityApiClient.Capabilities.PostAsync(name, description);
@@ -35,13 +36,30 @@ namespace CapabilityService.IntegrationTests.Features.Capabilities
         private async Task Then_it_wont_be_in_the_capability_list()
         {
             var capabilities = await CapabilityApiClient.Capabilities.GetAsync();
-            
+
             Assert.All(capabilities.Items, dto => Assert.False(dto.Id.Equals(_capabilityDto.Id)));
         }
 
         private void And_a_event_will_be_published()
         {
-            throw new System.NotImplementedException();
+            var capabilityKafkaClient = new CapabilityKafkaClient();
+          var events =  capabilityKafkaClient.GetUntilEventName(
+                WefoundTheEventWeWant, 
+                TimeSpan.FromSeconds(20)
+            );
+        }
+
+        public bool WefoundTheEventWeWant(dynamic @event)
+        {
+            if(@event == null) {return false;}
+            
+            if(
+                @event.eventName !="capability_deleted" || 
+                @event.payload.capabilityId != _capabilityDto.Id.ToString()
+            ) {return false;}
+
+
+            return true;
         }
     }
 }
