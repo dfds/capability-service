a80de383745599e083297de7a468511a190d76b0
Kim Lindhard <kilin@dfds.com>
2020-01-20 22:09:12 +0100

    add domain logic for naming topics

---
+++ src/CapabilityService.Tests/Features/Kafka/Domain/Models/TestTopicName.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using DFDS.CapabilityService.WebApi.Domain.Models;
+using DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Models;
+using Xunit;
+
+namespace DFDS.CapabilityService.Tests.Features.Kafka.Domain.Models
+{
+	public class TestTopicName
+	{
+		[Theory]
+		[InlineData("aa_aa", "aa-aa")]
+		[InlineData("!\"#¤%&/()=1a,", "1a")]
+		[InlineData("1234567890 abcdefghijklmnopqrstuvwxyzæøå.ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ!", "1234567890-abcdefghijklmnopqrstuvwxyzæøå-ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ")]
+		public void WillFormatName(string inputName, string expectedName)
+		{
+			var topicName = TopicName.Create(new CapabilityName("cap"), inputName);
+
+			var resultName = topicName.Name.Substring(topicName.Name.LastIndexOf('.') + 1);
+
+
+			Assert.Equal(expectedName, resultName);
+		}
+
+		[Fact]
+		public void WillTruncateAt255Chars()
+		{
+			var stingOf300As = new string(Enumerable.Repeat('A', 300).ToArray());
+
+			var topicName = TopicName.Create(new CapabilityName("cap"), stingOf300As);
+
+			Assert.Equal(255, topicName.Name.Length);
+		}
+	}
+}
--- src/CapabilityService.WebApi/Features/Kafka/Domain/Models/TopicName.cs
+++ src/CapabilityService.WebApi/Features/Kafka/Domain/Models/TopicName.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using DFDS.CapabilityService.WebApi.Domain.Models;
 
 namespace DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Models
@@ -11,6 +13,7 @@ namespace DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Models
 		}
 
 		public string Name { get; }
+
 		protected override IEnumerable<object> GetEqualityComponents()
 		{
 			yield return Name;
@@ -20,13 +23,38 @@ namespace DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Models
 		{
 			return new TopicName(fullName);
 		}
-			
+
 		public static TopicName Create(
-			CapabilityName capabilityName, 
+			CapabilityName capabilityName,
 			string topicName
 		)
 		{
-			return	new TopicName(capabilityName.Name + "." + topicName);
+			var cleanCapabilityName = CleanString(capabilityName.Name);
+			var cleanTopicName = CleanString(topicName);
+
+			var combinedString = cleanCapabilityName + "." + cleanTopicName;
+			var max255CharString = 255 < combinedString.Length ? 
+				combinedString.Substring(0, 255) :
+				combinedString;
+			
+			return new TopicName(max255CharString);
+		}
+
+		private static string CleanString(string input)
+		{
+			var inputLinted = input
+				.Replace(' ', '-')
+				.Replace('.', '-')
+				.Replace('_', '-');
+
+			var chars = inputLinted
+				.Where(c =>
+					Char.IsLetterOrDigit(c) ||
+					c == '-'
+				);
+
+
+			return new String(chars.ToArray());
 		}
 	}
 }
