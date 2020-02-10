1c86ddc7900e3627b9311010aefd648aafcba3bb
Emil H. Clausen <emcla@dfds.com>
2020-02-10 11:30:09 +0100

    Provide KafkaJanitor more data for SA flow

--- src/CapabilityService.WebApi/Features/Kafka/Domain/Services/ServiceAccountService.cs
+++ src/CapabilityService.WebApi/Features/Kafka/Domain/Services/ServiceAccountService.cs
@@ -1,6 +1,8 @@
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using DFDS.CapabilityService.WebApi.Domain.Models;
+using Newtonsoft.Json;
 
 namespace DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Services
 {
@@ -15,7 +17,19 @@ namespace DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Services
 		
 		public async Task EnsureServiceAccountAvailability(Capability capability, Topic topic)
 		{
-			var req = await _httpClient.PostAsync("http://localhost:5000/api/serviceaccount/request", new StringContent(""));
+			var payload = new ServiceAccountRequestInput
+			{
+				CapabilityName = capability.Name,
+				CapabilityId = capability.Id.ToString()
+			};
+			
+			var req = await _httpClient.PostAsync("http://localhost:5000/api/serviceaccount/request", new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
 		}
+	}
+	
+	public class ServiceAccountRequestInput
+	{
+		public string CapabilityName { get; set; }
+		public string CapabilityId { get; set; }
 	}
 }
