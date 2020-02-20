d1c29e19a73b671fa6211bc153455994aeee4537
Kim Lindhard <kilin@dfds.com>
2020-02-20 05:45:45 +0100

    Add KafkaJanitorRestClient

---
+++ src/CapabilityService.WebApi/Features/Kafka/Infrastructure/RestClients/KafkaJanitorRestClient.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using DFDS.CapabilityService.WebApi.Domain.Models;
+using KafkaJanitor.RestClient;
+using KafkaJanitor.RestClient.Features.Access.Models;
+using Topic = DFDS.CapabilityService.WebApi.Features.Kafka.Domain.Models.Topic;
+
+namespace DFDS.CapabilityService.WebApi.Features.Kafka.Infrastructure.RestClients
+{
+	public class KafkaJanitorRestClient : IKafkaJanitorRestClient
+	{
+		private readonly IRestClient _kafkaJanitorClient;
+
+		public KafkaJanitorRestClient(IRestClient kafkaJanitorClient)
+		{
+			_kafkaJanitorClient = kafkaJanitorClient;
+		}
+
+		public async Task CreateTopic(Topic topic, Capability capability)
+		{
+			
+			await _kafkaJanitorClient.Topics.CreateAsync(new KafkaJanitor.RestClient.Features.Topics.Models.Topic
+			{
+				Name = topic.Name.Name, 
+				Description = topic.Description, 
+				Partitions = topic.Partitions
+			});
+
+			await _kafkaJanitorClient.Access.RequestAsync(
+				new ServiceAccountRequestInput
+				{
+					CapabilityName = capability.Name, 
+					CapabilityId = capability.Id.ToString()
+				}
+			);
+		}
+	}
+}
